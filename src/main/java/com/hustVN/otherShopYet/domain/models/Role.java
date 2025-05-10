0641f242e807fcd56e3cb497d320bf75451fd877
HieuPahmTr <phamvanthang786@gmail.com>
2025-05-10 19:40:05 +0700

    add new models

--- src/main/java/com/hustVN/otherShopYet/domain/models/Role.java
+++ src/main/java/com/hustVN/otherShopYet/domain/models/Role.java
@@ -17,6 +17,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
 }
